fc5b51b6f477dcc2a03bc8918cf4b970f67d6f40
ninecrystal <tnwjd4230@naver.com>
2018-09-07 18:23:01 +0900

    Quize

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,13 @@
-var str = 'hello world';
-console.log(str);
+for(var count = 1; count < 101; count++){
+console.log(count);
+}
+
+for(var num = 1; num <101; num++){
+    if(num%2==1)
+    console.log(num);
+}
+
+for(var num1 = 1; num1<10;num1++){
+    for(var num2 = 1; num2<10; num2++)
+    console.log(num1 +"x" + num2 +"=" + num1*num2 );
+}
