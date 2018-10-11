6ae49a01ade7cd94ebe209e52d796cb75d6f447f
ninecrystal <tnwjd4230@naver.com>
2018-10-11 22:30:46 +0900

    검색기능완성

--- 06/app.js
+++ 06/app.js
@@ -1,18 +1,50 @@
 console.log('app')
 
 var appkey = '0ddb0a4e4d6b5d88ca888d32d820af80';
-var query = '제주대학교';
-var url = `https://dapi.kakao.com/v2/search/web?query=${query}`;
-
 var myHeaders = new Headers();
 myHeaders.append('Authorization','KakaoAK 0ddb0a4e4d6b5d88ca888d32d820af80')
 var options = {
     headers : myHeaders
 };
+var query;
+var pagesize;
 
-fetch(url, options)
-.then(function(response){
-    response.json().then(function(data){
-        console.log('json data:',data);
+function print(json){
+    console.log(json);
+
+    var str = '';
+
+    for(var i=0; i<json.documents.length; i++){
+        //console.log(json.data[i].title);
+        var title = json.documents[i].title;
+        var url = json.documents[i].url;
+        var contents = json.documents[i].contents;
+        console.log(title);
+        str += `<a href = ${url}>${title}</a><br>${contents}<br>`;
+    }
+
+    document.getElementById('wrap').innerHTML = str; // html파일에 
+}                                       //있는 'wrap'이라는 아이디를 가진 태그에
+                                        // str을 넣습니다
+                                        var query
+function getdata(query, options, callback){
+    var url = `https://dapi.kakao.com/v2/search/web?query=${query}&size=${pagesize}`;
+    fetch(url, options)
+    .then(function(response){
+        response.json().then(function(data){
+            callback(data);//getdata(print<< 이거를 가져온거야 
+        });                 //callback() -> print() 이렇게됨)
     });
-});
+}
+
+document.getElementById('button').addEventListener('click',function(){
+    query = document.getElementById("search").value;
+    pagesize=10;
+    getdata(query, options,print);
+})
+
+
+document.getElementById('moreview').addEventListener('click',function(){
+    pagesize += 5;
+    getdata(query, options,print);
+})
