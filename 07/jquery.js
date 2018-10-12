1a15bc282cca349677b00d3336e31d4f8225d554
ninecrystal <tnwjd4230@naver.com>
2018-10-12 18:12:42 +0900

    수업내용과스크롤

---
+++ 07/jquery.js
@@ -0,0 +1,32 @@
+//document.querySelector('h1').innerHTML = 'hi';
+//$('h1').fadeOut().delay(1000).fadeIn();
+
+//var $div = $('div')
+
+//$div
+//    .css('background-color', 'yellow')
+//    .css('border','1px solid red');
+//var divs = document.querySelectorAll('div');
+//for(var i = 0; i< divs.length; i++) {
+//    divs[i].style.backgroundColor = 'red';
+//}
+
+var $btn = $('#btn');
+var $txt = $('#txt');
+var $debug = $('#debug');
+
+$btn.on('click', function(event){
+    console.log('click');
+
+    $('h1').hide('slow');//fadeout  cf)hide 대신에 toggle은 반복
+    // if($txt.val()===''){
+    //     alert('값을 넣어주세요')
+    //     return;
+    // }
+    // $debug.html($txt.val());
+});
+
+$('wrap').on('click', 'div', function(event){
+    $(event.target).fadeOut();
+    console.log(event.target);
+});
